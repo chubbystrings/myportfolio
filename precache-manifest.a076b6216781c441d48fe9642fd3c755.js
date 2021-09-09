a8a9d78e3cce1e0467832229d0a812849e64d7a3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-09-09 00:19:50 +0000

    Deploy chubbystrings/myportfolio to github.com/chubbystrings/myportfolio.git:gh-pages

---
+++ precache-manifest.a076b6216781c441d48fe9642fd3c755.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffa656d8597565ef7d10",
+    "url": "/portfolio/css/app.813c71b1.css"
+  },
+  {
+    "revision": "3b01fcec80571a1b4ab5",
+    "url": "/portfolio/css/chunk-vendors.3c62b671.css"
+  },
+  {
+    "revision": "d2d68327fcc26c9fb5611f4ff71d0f9e",
+    "url": "/portfolio/img/artoftravel.d2d68327.jpg"
+  },
+  {
+    "revision": "61acfaff07b4ef79463e1f96b028a5be",
+    "url": "/portfolio/img/myphoto.61acfaff.png"
+  },
+  {
+    "revision": "afb31f8e82a8a5b964fc390cdb2131fd",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ffa656d8597565ef7d10",
+    "url": "/portfolio/js/app.c47bf1ab.js"
+  },
+  {
+    "revision": "3b01fcec80571a1b4ab5",
+    "url": "/portfolio/js/chunk-vendors.270c2244.js"
+  },
+  {
+    "revision": "1770c2524b653c4741436956a1f7f256",
+    "url": "/portfolio/manifest.json"
+  },
+  {
+    "revision": "80bb08c20d4c99c116af3b8610c96715",
+    "url": "/portfolio/my.ico"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/portfolio/robots.txt"
+  }
+]);
